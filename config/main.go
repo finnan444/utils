3be255c11e9526bf16f6c26cb527ee2293254b2d
Vlad Ryk <rykovsky@pik-arenda.ru>
2019-08-19 15:23:46 +0300

    load configuration method enhanced

--- config/main.go
+++ config/main.go
@@ -3,12 +3,27 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
 
 // LoadConfiguration универсальный загрузчик конфы
-func LoadConfiguration(filename string, config interface{}) error {
-	file, err := os.Open(filename)
-	if err != nil {
+func LoadConfiguration(mode string, config interface{}) error {
+	var (
+		name     = "conf.json"
+		fileName string
+		file     *os.File
+		err      error
+	)
+
+	if mode == "prod" {
+		name = "conf_prod.json"
+	}
+
+	if fileName, err = filepath.Abs(filepath.Dir("conf/" + name)); err != nil {
+		return err
+	}
+
+	if file, err = os.Open(fileName); err != nil {
 		return err
 	}
 	defer file.Close()
